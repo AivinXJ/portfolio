76cb858a1ee394011dc488742e2c98432963a718
AivinXJ <aidly123.game@gmail.com>
2025-08-16 18:48:56 +0300

    Updates

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var vite_config_default = defineConfig({
     ] : []
   ],
   // Remove the trailing slash from the base path
-  base: "/portfolio/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
